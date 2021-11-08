cceba1b317b56b91f0f8c93b998b7e67645bf081
kulius <kulius@msn.com>
2021-11-08 09:25:08 +0800

    20211108 增加說明及加入 Expense Tier Validation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # odoo13_smobio
-smobio
+##原生模組安裝
+###- 員工、費用、庫存、會計、銷售、採購、
+##第三方模組安裝
+- employee_expense_advance (預用預支模組)(odoo10升級)
+- Base Tier Validation (基礎簽核模組)
+- Base Tier Validation Formula (簽核可寫入程式)
+- Base Tier Validation Forward  
+- Expense Tier Validation (費用簽核)
+- Purchase Tier Validation (採購簽核)
+- Purchase Request Tier Validation (採購請求簽核) 
+- Sale Tier Validation (銷售簽核)
+
+- (oca)Account - Missing Menus (顯示財務設定選單)
+- (oca)Account Financial Reports (財務報表)
+- (oca)Advanced search (進階蒐尋可使用o2m的條件)
+- (oca)Base report xlsx
+- (oca)Date Range (財務報表中 日期的快速下拉)
+
+### 佈景
+- Dependency for backend_theme_v13
--- addons_smobio/base_tier_validation/models/tier_definition.py
+++ addons_smobio/base_tier_validation/models/tier_definition.py
@@ -23,19 +23,32 @@ class TierDefinition(models.Model):
         default=lambda self: self._get_default_name(),
         translate=True,
     )
-    model_id = fields.Many2one(comodel_name="ir.model", string="Referenced Model")
+    model_id = fields.Many2one(
+        comodel_name="ir.model",
+        string="Referenced Model",
+        domain=lambda self: [("model", "in", self._get_tier_validation_model_names())],
+    )
     model = fields.Char(related="model_id.model", index=True, store=True)
     review_type = fields.Selection(
         string="Validated by",
         default="individual",
         selection=[
             ("individual", "Specific user"),
-            ("group", "Any user in a specific group."),
+            ("group", "Any user in a specific group"),
+            ("field", "Field in related record"),
         ],
     )
     reviewer_id = fields.Many2one(comodel_name="res.users", string="Reviewer")
     reviewer_group_id = fields.Many2one(
         comodel_name="res.groups", string="Reviewer group"
+    )
+    reviewer_field_id = fields.Many2one(
+        comodel_name="ir.model.fields",
+        string="Reviewer field",
+        domain="[('id', 'in', valid_reviewer_field_ids)]",
+    )
+    valid_reviewer_field_ids = fields.One2many(
+        comodel_name="ir.model.fields", compute="_compute_domain_reviewer_field",
     )
     definition_type = fields.Selection(
         string="Definition", selection=[("domain", "Domain")], default="domain"
@@ -46,7 +59,7 @@ class TierDefinition(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
-        default=lambda self: self.env.user.company_id,
+        default=lambda self: self.env.company,
     )
     notify_on_create = fields.Boolean(
         string="Notify Reviewers on Creation",
@@ -60,15 +73,14 @@ class TierDefinition(models.Model):
         help="Approval order by the specified sequence number",
     )
 
-    @api.onchange("model_id")
-    def onchange_model_id(self):
-        return {
-            "domain": {
-                "model_id": [("model", "in", self._get_tier_validation_model_names())]
-            }
-        }
-
     @api.onchange("review_type")
     def onchange_review_type(self):
         self.reviewer_id = None
         self.reviewer_group_id = None
+
+    @api.depends("review_type", "model_id")
+    def _compute_domain_reviewer_field(self):
+        for rec in self:
+            rec.valid_reviewer_field_ids = self.env["ir.model.fields"].search(
+                [("model", "=", rec.model), ("relation", "=", "res.users")]
+            )
