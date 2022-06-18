1b97ef24eebfe9beb58c407ac333ca9a6667b293
at2166 <Mydream23>
2022-06-18 18:46:27 +0800

    20220618 費用單據調整

--- addons_smobio/smobio_udm/models/hr_expense_sheet.py
+++ addons_smobio/smobio_udm/models/hr_expense_sheet.py
@@ -23,3 +23,28 @@ class HrExpenseSheet(models.Model):
     #             # rec.action_submit_sheet()
     #             rec.write({'state': 'approve'})
     #             rec.activity_update()
+
+class HrExpense(models.Model):
+    _inherit = "hr.expense"
+
+    @api.model
+    def _get_employee_id_domain(self):
+        res = "['|', ('company_id', '=', False), ('company_id', '=', company_id)]"  # Then, domain accepts everything
+        # res = [('id', '=', 0)] # Nothing accepted by domain, by default
+        # if self.user_has_groups('hr_expense.group_hr_expense_user') or self.user_has_groups('account.group_account_user'):
+        #     res = "['|', ('company_id', '=', False), ('company_id', '=', company_id)]"  # Then, domain accepts everything
+        # elif self.user_has_groups('hr_expense.group_hr_expense_team_approver') and self.env.user.employee_ids:
+        #     user = self.env.user
+        #     employee = self.env.user.employee_id
+        #     res = [
+        #         '|', '|', '|',
+        #         ('department_id.manager_id', '=', employee.id),
+        #         ('parent_id', '=', employee.id),
+        #         ('id', '=', employee.id),
+        #         ('expense_manager_id', '=', user.id),
+        #         '|', ('company_id', '=', False), ('company_id', '=', employee.company_id.id),
+        #     ]
+        # elif self.env.user.employee_id:
+        #     employee = self.env.user.employee_id
+        #     res = [('id', '=', employee.id), '|', ('company_id', '=', False), ('company_id', '=', employee.company_id.id)]
+        return res
